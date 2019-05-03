e176b2b1e6d9a2c5b4dee407fa8bc23926147b9e
Sean Kleinjung <skleinjung@gmail.com>
2019-05-02 22:03:53 -0500

    Rename some flux manifests. Add Prometheus operator manifests to Bootstrap.

--- init/bin/gen-bootstrap.sh
+++ init/bin/gen-bootstrap.sh
@@ -106,17 +106,19 @@ function generate_templated_yaml() {
     find output/yaml -name *.yaml -exec sed -i -e "s,\${GIT_PATH},${GIT_PATH}," \{\} \;
 }
 
-function generate_helm_yaml() {
-    echo Generating yaml files from Helm charts...
+function generate_flux_yaml() {
+    echo Generating Flux files from Helm charts...
 
     rm -rf output/tmp
-    mkdir -p output/tmp/helm
-    mkdir -p output/tmp/output
-    git clone https://github.com/weaveworks/flux.git output/tmp/flux -b ${FLUX_VERSION}
+    mkdir -p output/tmp/charts
+    mkdir -p output/tmp/gen
+
+    helm repo add weaveworks https://weaveworks.github.io/flux
+    helm fetch --untar --untardir output/tmp/charts 'weaveworks/flux'
 
     helm template -n flux-master \
         --namespace kube-system \
-        --output-dir output/tmp/helm \
+        --output-dir output/tmp/gen \
         --set git.url=${GIT_URL} \
         --set git.branch=${GIT_BRANCH} \
         --set git.path=${GIT_PATH} \
@@ -127,12 +129,41 @@ function generate_helm_yaml() {
         --set rbac.create=false \
         --set serviceAccount.create=false \
         --set serviceAccount.name=flux-master \
-        output/tmp/flux/chart/flux
+        output/tmp/charts/flux
 
-    find output/tmp/helm/flux/templates -name *.yaml -exec sed \{\} -i -e "/^\  name: .*/ a\
+    find output/tmp/gen/flux/templates -name *.yaml -exec sed \{\} -i -e "/^\  name: .*/ a\
 \  namespace: flux-master" \;
 
-    cp output/tmp/helm/flux/templates/*.yaml output/yaml
+    mkdir -p output/yaml/flux
+    cp output/tmp/gen/flux/templates/*.yaml output/yaml/flux
+    rm -rf output/tmp
+}
+
+function generate_prometheus_yaml() {
+    echo Generating Prometheus operator files from Helm charts...
+
+    rm -rf output/tmp
+    mkdir -p output/tmp/work
+#    mkdir -p output/tmp/charts
+#    mkdir -p output/tmp/gen
+
+#    helm fetch --untar --untardir output/tmp/charts 'stable/prometheus-operator'
+#    helm template -n prometheus-operator \
+#        --namespace prometheus \
+#        --output-dir output/tmp/gen \
+#        output/tmp/charts/prometheus-operator
+#
+#    find output/tmp/prometheus/flux/templates -name *.yaml -exec sed \{\} -i -e "/^\  name: .*/ a\
+#\  namespace: flux-master" \;
+#
+#    mkdir -p output/yaml/flux
+#    cp output/tmp/prometheus/flux/templates/*.yaml output/yaml/flux
+#    rm -rf output/tmp
+
+    git clone https://github.com/coreos/prometheus-operator.git -b v0.29.0 output/tmp/work
+
+    mkdir -p output/yaml/prometheus
+    cp -r output/tmp/work/contrib/kube-prometheus/manifests/*.yaml output/yaml/prometheus
     rm -rf output/tmp
 }
 
@@ -141,4 +172,7 @@ process_arguments $@
 rm -rf output
 mkdir output
 generate_templated_yaml
-generate_helm_yaml
+
+helm init --client-only
+generate_flux_yaml
+generate_prometheus_yaml
